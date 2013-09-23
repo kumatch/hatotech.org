28edcea44192b69bde640f35172bd2e340dc465c
Yosuke Kumakura <kumatch@gmail.com>
2013-09-23 22:33:48 +0900

    9/x diaries.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var connect = require('connect');
 var web_root = __dirname + "/web";
 
-connect.createServer( connect.static(web_root) ).listen(8080);
+connect.createServer( connect.static(web_root) ).listen(3000);
